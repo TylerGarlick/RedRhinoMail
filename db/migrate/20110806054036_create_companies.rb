006ce6728816975c9a8ec3c033cfbcf7a58810ba
Tyler Garlick <tjgarlick@gmail.com>
2011-08-17 23:08:55 -0600

    stuff

--- db/migrate/20110806054036_create_companies.rb
+++ db/migrate/20110806054036_create_companies.rb
@@ -1,7 +1,10 @@
 class CreateCompanies < ActiveRecord::Migration
   def change
     create_table :companies do |t|
-
+      t.string :name
+      t.string :phone
+      t.string :website
+      t.boolean :is_active
       t.timestamps
     end
   end
